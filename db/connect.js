64794a5b17d7222debc66a67b087364bff8b452a
Farook <farook.jintha@ace-alternatives.com>
2022-11-06 12:08:57 +0530

    modified mongo URL

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 db = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_LOCAL);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('DB Connection established.');
     }catch(error){
         console.log('Error while connecting DB: ', error);
